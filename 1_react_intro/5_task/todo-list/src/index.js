535614c3afbb1503c0fa61b8dd858488376d2889
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-13 00:31:12 +0200

    add task 5

---
+++ 1_react_intro/5_task/todo-list/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootElem = document.querySelector('#root');
+
+const titleElem = <h1 className="title">TodoList</h1>;
+
+ReactDOM.render(titleElem, rootElem);
