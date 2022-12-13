f961eacd8dc8d793af4e2cbc72ed2dcd89fd85bc
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-13 12:09:28 +0200

    + fix

--- 3_components-props/1_task/src/Greeting.jsx
+++ 3_components-props/1_task/src/Greeting.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Greeting = (props) => (
   <div className="greeting">{`My name is ${props.firstName}  ${
     props.lastName
-  }. I am ${new Date(new Date().getTime() - props.birthDate.getTime()).getFullYear() - 1970} years old`}</div>
+  }. I am ${new Date().getFullYear() - props.birthDate.getFullYear()} years old`}</div>
 );
 
 export default Greeting;
