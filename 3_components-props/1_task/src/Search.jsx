f237dec328b53e6f3b3e9643f866fec7c1ad3b1a
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-13 07:46:34 +0200

    add task 2 (used base from task 1)

--- 3_components-props/1_task/src/Search.jsx
+++ 3_components-props/1_task/src/Search.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default (props) => (
-  <div className="search">
-    <h1 className="search__title">{`Hello, ${props.name}. What to search for you?`}</h1>
-    <div className="search__field">
-      <input type="text" className="search__input" />
-      <button className="search__button">Search</button>
-    </div>
-  </div>
-);
+export default class Search extends Component {
+    render() {
+        return (
+          <div className="search">
+            <h1 className="search__title">{`Hello, ${this.props.name}. What to search for you?`}</h1>
+            <div className="search__field">
+              <input type="text" className="search__input" />
+              <button className="search__button">Search</button>
+            </div>
+          </div>
+        );
+    }
+}
