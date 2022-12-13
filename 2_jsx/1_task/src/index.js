ec2b55e490dd76e857c6a2165e96e9e607256c21
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-13 07:00:10 +0200

    add task 3 (base task 1-2)

--- 2_jsx/1_task/src/index.js
+++ 2_jsx/1_task/src/index.js
@@ -4,39 +4,20 @@ import './index.css';
 
 const rootElem = document.querySelector('#root');
 
-const newBlock = (
-  <main className="page">
-    <form className="login-form">
-      <h1 className="form-title">Profile</h1>
-      <div className="form-control">
-        <label className="form-label" htmlFor="name">
-          Name
-        </label>
-        <input
-          className="form-input"
-          type="text"
-          id="name"
-          name="name"
-          defaultValue="anonymous"
-        />
-      </div>
-      <div className="form-control">
-        <label className="form-label" id="age" htmlFor="age">
-          Age
-        </label>
-        <input
-          className="form-input"
-          type="number"
-          defaultValue="17"
-          name="age"
-        />
-        <span style={{ color: 'red', fontWeight: 700 }}>To young</span>
-      </div>
-      <button className="submit-button" type="submit">
-        Submit
-      </button>
-    </form>
-  </main>
-);
+const secondsRender = () => {
+  const currentSecond = new Date().getSeconds();
+  const secondsElem = (
+    <div
+      className="seconds"
+      style={{
+        color: currentSecond % 2 === 0 ? '#fff' : '#000',
+        backgroundColor: currentSecond % 2 !== 0 ? '#fff' : '#000',
+      }}
+    >
+      {currentSecond}
+    </div>
+  );
+  ReactDOM.render(secondsElem, rootElem);
+};
 
-ReactDOM.render(newBlock, rootElem);
+setInterval(() => secondsRender(), 1000);
