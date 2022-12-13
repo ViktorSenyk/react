05f58bc7bd5b78e4f52cff0a4a225bb57d86e43b
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-13 12:41:41 +0200

    test

--- 3_components-props/1_task/src/Greeting.jsx
+++ 3_components-props/1_task/src/Greeting.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import moment from 'moment';
+// import moment from 'moment';
 
 const Greeting = (props) => (
   <div className="greeting">{`My name is ${props.firstName} ${
     props.lastName
-  }. I'm ${moment().diff(props.birthDate, 'years')} years old`}</div>
+  }. I'm ${new Date().getFullYear() - props.birthDate.getFullYear()} years old`}</div>
 );
 
 export default Greeting;
