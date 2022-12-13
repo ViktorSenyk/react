97c0fd4d1d10054b9395428c5d8ae5e1cf9cc253
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-13 12:16:34 +0200

    + moment

--- 3_components-props/1_task/src/Greeting.jsx
+++ 3_components-props/1_task/src/Greeting.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import moment from 'moment';
+
+const formatDate = (date) => moment(date).format('YYYY');
 
 const Greeting = (props) => (
   <div className="greeting">{`My name is ${props.firstName}  ${
     props.lastName
-  }. I am ${new Date().getFullYear() - props.birthDate.getFullYear()} years old`}</div>
+  }. I am ${
+    formatDate(new Date()) - formatDate(props.birthDate)
+  } years old`}</div>
 );
 
 export default Greeting;
