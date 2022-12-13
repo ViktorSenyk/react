adb6940286ce49a31b8479508b891e8f45ca9ce7
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-13 09:33:57 +0200

    + fix

--- 3_components-props/1_task/src/UserInfo.jsx
+++ 3_components-props/1_task/src/UserInfo.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 import UserAvatar from './Avatar';
 import './userInfo.scss'
 
-export default (props) => (
+ const UserInfo = (props) => (
   <div className="user-info">
     <UserAvatar avatarUrl={props.user.avatarUrl} name={props.user.name} />
     <div className="user-info__name">{props.user.name}</div>
   </div>
 );
+
+export default UserInfo;
