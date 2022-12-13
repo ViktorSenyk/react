13067f45663c43d950619482c9fad9c2cafd7343
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-13 10:10:53 +0200

    + fix

--- 3_components-props/1_task/src/index.jsx
+++ 3_components-props/1_task/src/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import Greeting from './Greeting';
 
 const rootElem = document.querySelector('#root');
 
-ReactDOM.render(<App />, rootElem);
+ReactDOM.render(
+  <Greeting
+    firstName="John"
+    lastName="Due"
+    birthDate={new Date().getFullYear() - new Date('2005').getFullYear()}
+  />,
+  rootElem
+);
