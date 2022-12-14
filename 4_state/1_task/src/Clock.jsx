883946e422713f0e0a72e789d754a772c5f2263a
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-14 19:43:03 +0200

    + fix

--- 4_state/1_task/src/Clock.jsx
+++ 4_state/1_task/src/Clock.jsx
@@ -11,7 +11,8 @@ class Clock extends Component {
     };
 
     function getTimeWithOffset(offset) {
-      const currentTime = new Date(new Date().setHours(new Date().getHours() + 2));
+      // const currentTime = new Date(new Date().setHours(new Date().getHours() + 2));
+      const currentTime = new Date();
       const utcOffset = currentTime.getTimezoneOffset() / 60;
       return new Date(
         currentTime.setHours(currentTime.getHours() + offset + utcOffset)
@@ -29,9 +30,9 @@ class Clock extends Component {
     return (
       <div className="clock">
         <div className="clock__location">{this.props.location}</div>
-        <div className="clock__time">{`${moment(this.state.time).format(
-          'h:mm:ss a'
-        )}`}</div>
+        <div className="clock__time">{moment(this.state.time).format(
+          'h:mm:ss A'
+        )}</div>
       </div>
     );
   }
