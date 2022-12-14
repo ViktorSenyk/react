c46078e408b573fa4dfb70e6df66cb8940d6d4d7
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-14 19:54:38 +0200

    + fix

--- 4_state/1_task/src/Clock.jsx
+++ 4_state/1_task/src/Clock.jsx
@@ -7,21 +7,20 @@ class Clock extends Component {
     super(props);
 
     this.state = {
-      time: moment().format('h:hh:ss a'),
+      time: new Date(),
     };
 
-    function getTimeWithOffset(offset) {
-      const currentTime = new Date(new Date().setHours(new Date().getHours() + 2));
-      // const currentTime = new Date();
+    const getTimeWithOffset = (offset) => {
+      const currentTime = new Date();
       const utcOffset = currentTime.getTimezoneOffset() / 60;
       return new Date(
         currentTime.setHours(currentTime.getHours() + offset + utcOffset)
       );
-    }
+    };
 
     setInterval(() => {
       this.setState({
-        time: getTimeWithOffset(props.offset),
+        time: getTimeWithOffset(this.props.offset),
       });
     }, 1000);
   }
@@ -30,9 +29,9 @@ class Clock extends Component {
     return (
       <div className="clock">
         <div className="clock__location">{this.props.location}</div>
-        <div className="clock__time">{moment(this.state.time).format(
-          'h:mm:ss A'
-        )}</div>
+        <div className="clock__time">
+          {moment(this.state.time).format('h:mm:ss A')}
+        </div>
       </div>
     );
   }
