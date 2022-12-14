fed642196f6f86fbef9d01a9383dff080b84d86f
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-14 19:45:48 +0200

    + fix

--- 4_state/1_task/src/Clock.jsx
+++ 4_state/1_task/src/Clock.jsx
@@ -11,7 +11,7 @@ class Clock extends Component {
     };
 
     function getTimeWithOffset(offset) {
-      const currentTime = new Date(new Date().setHours(new Date().getHours() - 2));
+      const currentTime = new Date(new Date().setHours(new Date().getHours() + 2));
       // const currentTime = new Date();
       const utcOffset = currentTime.getTimezoneOffset() / 60;
       return new Date(
