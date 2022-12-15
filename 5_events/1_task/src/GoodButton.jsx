70aadebb53fb3d893399a643edc9df0b6651f028
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-15 02:00:33 +0200

    add task 2

--- 5_events/1_task/src/GoodButton.jsx
+++ 5_events/1_task/src/GoodButton.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 
 class GoodButton extends Component {
+  clickFunc(e) {
+    alert(e.target.textContent);
+  }
   render() {
     return (
-      <button className="fancy-button" onClick={() => alert('Good job!')}>
+      <button className="fancy-button" onClick={this.clickFunc}>
         Click me!
       </button>
     );
