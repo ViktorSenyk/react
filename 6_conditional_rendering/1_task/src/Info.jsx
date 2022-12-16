6f782a9eab39372294cd291c2af9c498b633ea5c
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-16 18:32:55 +0200

    + text

--- 6_conditional_rendering/1_task/src/Info.jsx
+++ 6_conditional_rendering/1_task/src/Info.jsx
@@ -2,5 +2,6 @@ import React, { Component } from 'react';
 
 export default class Info extends Component {
   render = () =>
-    !this.props.info ? null : <div className="message">{this.props.info}</div>;
+    // !this.props.info ? null : <div className="message">{this.props.info}</div>;
+    !this.props.info ? null : <div className="info">{this.props.info}</div>;
 }
