989137ce542412c964b23ee40473f5d7abc02d9f
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-22 04:30:02 +0200

    add 9_forms => task 1

---
+++ 9_forms/1_task/src/App.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import Search from './Search';
+
+export default class App extends Component {
+  render = () => (
+    <>
+      <Search />
+    </>
+  );
+}
