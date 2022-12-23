2a75c5b75e968e35728c9a07d1307271a165bb91
ViktorSenyk <victor.senyk.vs@gmail.com>
2022-12-23 10:25:18 +0200

    add task 5

--- 9_forms/1_task/src/index.jsx
+++ 9_forms/1_task/src/index.jsx
@@ -1,11 +1,11 @@
 import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import Game from './Game';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Game />
   </React.StrictMode>
 );
