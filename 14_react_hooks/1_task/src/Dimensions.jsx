077e9f346e37e55fbd6799245c17806b60542845
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-01-08 11:10:48 +0200

    + fix

--- 14_react_hooks/1_task/src/Dimensions.jsx
+++ 14_react_hooks/1_task/src/Dimensions.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react';
 
 export default function Dimensions() {
-  const [size, setSize] = useState({ width: null, height: null });
+  const [dimensions, setDimensions] = useState({ width: null, height: null });
 
   useEffect(() => {
-    setSize({ width: window.innerWidth, height: window.innerHeight });
-    const handleResize = (e) =>
-      setSize({ width: e.target.innerWidth, height: e.target.innerHeight });
+    const { innerHeight, innerWidth } = window;
+    setDimensions({ width: innerWidth, height: innerHeight });
+    const handleResize = (e) => {
+      const { innerWidth, innerHeight } = e.target;
+      setDimensions({ width: innerWidth, height: innerHeight });
+    };
     window.addEventListener('resize', handleResize);
     return () => {
       window.removeEventListener('resize', handleResize);
     };
   }, []);
 
+  const { width, height } = dimensions;
   return (
     <div className="dimensions">
-      `${size.width}px - ${size.height}px`
+      `${width}px - ${height}px`
     </div>
   );
 }
