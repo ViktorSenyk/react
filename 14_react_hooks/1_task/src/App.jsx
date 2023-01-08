9920a1d0662ea4a5a89638ce81f85d92987d3f6e
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-01-08 10:26:57 +0200

    add task 2

--- 14_react_hooks/1_task/src/App.jsx
+++ 14_react_hooks/1_task/src/App.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
-import Counter from './Counter';
+import User from './User';
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 
 export default function App() {
-  return <Counter />;
+  return (
+    <div className="page">
+      <BrowserRouter>
+        <div className="page__content">
+          <h1>Users</h1>
+          <ul className="navigation">
+            <li className="navigation__item">
+              <Link to="/users/github">Github</Link>
+            </li>
+            <li className="navigation__item">
+              <Link to="/users/facebook">Facebook</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route exact path="/">
+              <span>Select a user please</span>
+            </Route>
+            <Route path="/users/:userId">
+              <User />
+            </Route>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
