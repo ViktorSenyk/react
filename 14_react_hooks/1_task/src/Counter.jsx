93b1b6fb231173c29bae9d1c9bd7abbc1ceb8f4b
ViktorSenyk <victor.senyk.vs@gmail.com>
2023-01-08 09:25:10 +0200

    add 14_react_hooks => task 1

---
+++ 14_react_hooks/1_task/src/Counter.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div className="counter">
+      <button className="counter__button" onClick={() => setCount(count - 1)}>
+        -
+      </button>
+      <span className="counter__value">{count}</span>
+      <button className="counter__button" onClick={() => setCount(count + 1)}>
+        +
+      </button>
+    </div>
+  );
+}
